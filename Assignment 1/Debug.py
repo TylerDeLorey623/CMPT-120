3ee05f7c543dac8256c8c11de4a264002e8be354
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2022-09-08 08:43:35 -0400

    assignment 1

--- Assignment 1/Debug.py
+++ Assignment 1/Debug.py
@@ -1,33 +1,28 @@
-
-#Assignment 1: Debug.py
-#Fix any syntax errors you can spot and fulfill any other requests
+# Assignment 1: Debug.py
+# Fix any syntax errors you can spot and fulfill any other requests
 def main():
-    print(hello)
+    print("hello")
     print(3)
     print(10 + 10)
 
+    # insert your name in this string below
+    print("My name is Tyler")
+    # this is printing a string and not an integer. how do we fix it?
+    print(50)
+    # make sure this one prints out the sum
+    print(2 + 2)
 
-    #insert your name in this string below
-    print("My name is ")
-    #this is printing a string and not an integer. how do we fix it?
-    print("50")
-    #make sure this one prints out the sum
-    print("2 + 2")
+    # insert your name into this variable
+    name = "Tyler"
+    print("Hello, my name is", name)
 
-    #insert your name into this variable
-    name = ""
-    print("Hello, my name is ", name)
+    # insert your best friend's name here and have it print out
+    name2 = "Ian"
+    print("Hello, my best friend's name is", name2)
 
-    #insert your best friend's name here and have it print out
-    name2 = ""
-    print("Hello, my best friend's name is ", name)
-    
-    #put your age here
-    age = 
-    print("I'm " + name + " and i'm " + str(age) + " years old")
-    
- 
-   
+    # put your age here
+    age = 18
+    print("I'm " + name + " and I'm " + str(age) + " years old")
+
 
 main()
-
--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -1,16 +1,14 @@
-#instructions: print out "Hello World!"
-#create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
-#create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
-
+# instructions: print out "Hello World!"
+# create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
+# create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
 
 
 def main():
+    print("Hello World!")
+    favColor = "orange"
+    print("My favorite color is: " + favColor)
+    birthday = 62304
+    print("My birthday is: " + str(birthday))
 
 
-
-    
-    
-    
-    
-    
 main()
