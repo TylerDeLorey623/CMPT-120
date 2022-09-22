6fba48859ed781d215229d0c3eba2513d78ae19b
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2022-09-22 09:20:35 -0400

    assignment 2.5

--- Assignment 2.5/double_int_list_practice.py
+++ Assignment 2.5/double_int_list_practice.py
@@ -1,29 +1,38 @@
 def main():
-  
   #set this to any double
-  doubleValue =
+  doubleValue = 3.14
   
   #set this to any int
-  intValue = 
+  intValue = 10
   
   #print out addition, subtraction, multiplication, and division of these two values
-  
-  
+
+  print("Addition:", intValue + doubleValue)
+  print("Subtraction:", intValue - doubleValue)
+  print("Multiplication:", intValue * doubleValue)
+  print("Division:", intValue / doubleValue)
   
   #populate this list
-  myFriends = []
+  myFriends = ["Chris", "Kristen", "Lauren", "Daniel", "Ian"]
   
   #print out your friends at index 2 and index 3
-  
+  print(myFriends[2])
+  print(myFriends[3])
   
   #populate this list with five numbers
-  fiveNumbers = []
+  fiveNumbers = [10, 20, 30, 40, 50]
   
   #do each of the four equations with different numbers each time.
-  
+  print("Addition:", fiveNumbers[2] + fiveNumbers[4])
+  print("Subtraction:", fiveNumbers[1] - fiveNumbers[0])
+  print("Multiplication:", fiveNumbers[0] * fiveNumbers[3])
+  print("Division:", fiveNumbers[3] / fiveNumbers[1])
   #now replace two of the numbers in the list with a different number (using name of list[x] = ?, not rewriting the fiveNumber list)
-  
+  fiveNumbers[3] = 60
+  fiveNumbers[4] = 100
+
   #print out the list
-  
-  
+  print(fiveNumbers)
+
+
 main()
--- Assignment 2.5/if_else_sign_practice.py
+++ Assignment 2.5/if_else_sign_practice.py
@@ -5,7 +5,17 @@ def main():
         If they're 18 and older but younger than 21, tell them they can only buy nicotine products in some states
         If they're less than 18, they can only purchase candy cigarettes and sody pops
     '''
-    
+
+    age = input("How old are you? ")
+    age = int(age)
+    if age >= 25:
+        print("You can buy alcohol, nicotine products, and you can rent a car.")
+    elif age >= 21:
+        print("You can buy alcohol and nicotine products, but you cannot rent a car.")
+    elif age >= 18:
+        print("You can only buy nicotine products in some states.")
+    else:
+        print("You can only purchase candy cigarettes and sody pops.")
   
 
   
