a4a218c3f948de324f162efe2e441f061d662277
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2022-10-25 15:03:35 -0400

    assignment 5

--- Assignment 5/Debug.py
+++ Assignment 5/Debug.py
@@ -1,38 +1,53 @@
 def main():
-    #good working example!
-    stringInput = input("enter a string")
+    # good working example!
+    stringInput = input("enter a string ")
     if stringInput.isalpha():
         print("string!")
     else:
         print("not string :(")
-        
-    #can you google and find what function you should use to check if it's numeric (an int?)?
-    intInput = input("enter an int")
-    if intInput.?:
+
+    # can you google and find what function you should use to check if it's numeric (an int?)?
+    intInput = input("enter an int ")
+    if intInput.isnumeric():
         print("int!")
     else:
         print("not int :(")
-    
-    #what about if it's both letters and numbers?
-    alphIntInput = input("Enter letters and numbers")
-    if alphIntInput.?:
+
+    # what about if it's both letters and numbers?
+    alphIntInput = input("Enter letters and numbers ")
+    if alphIntInput.isalnum():
         print("Letters and numbers!")
     else:
         print("weird characters :(")
-       
-    #good working example
-    asterisk = input("Enter an asterisk please")
+
+    # good working example
+    asterisk = input("Enter an asterisk please ")
     if asterisk == "*":
         print("good!")
     else:
         print("not asterisk :(")
-        
-    #now write code to check if the input was either an asterisk OR an ampersand (&)
-    
-        
-    #do the live example we did in class: ask user to input an integer, but before you cast it to an int, check that it's an integer before doing your variable = int(variable) command
-    
+
+    # now write code to check if the input was either an asterisk OR an ampersand (&)
+    char = input("Enter an asterisk or ampersand ")
+    if char == "*" or char == "&":
+        print("Good job. You entered a", char)
+    else:
+        print("You did a bad job.")
+
+    # do the live example we did in class: ask user to input an integer, but before you cast it to an int, check that it's an integer before doing your variable = int(variable) command
+    num = input("Enter a integer: ")
+    if num.isnumeric():
+        num = int(num)
+        print(num)
+    else:
+        print("You didn't input an integer.")
     # last challenge: find out how to check if the string input has the substring "marist"
-    #google this one ;) substring is the key google term
-    
+    # google this one ;) substring is the key google term
+    maristString = input("Enter a string that has the substring 'marist' ")
+    if "marist" in maristString:
+        print("Good job!")
+    else:
+        print("Wrong.")
+
+
 main()
