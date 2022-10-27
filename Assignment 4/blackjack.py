213b9cc14901e55f60f3da525acf848b0b63e6cd
TylerDeLorey623 <112398227+TylerDeLorey623@users.noreply.github.com>
2022-10-27 08:12:25 -0400

    assignment 4

--- Assignment 4/blackjack.py
+++ Assignment 4/blackjack.py
@@ -1,6 +1,7 @@
 #if you don't know how to play blackjack, tbh not super important but look it up anyway LOL
 #this script is going to require some googling: I want you to practice using your resources with this one. But of course if you get stuck, reach out :)
 '''instructions: randomly generate three values between 1 and 11. in the function bust: add these three numbers together. if they add up to or less than 21, return the sum. If it's over 21, return 0. If it's over 21 BUT there's an 11 as one of the values, return the sum - 10. '''
+import random
 
 
 def bust(x, y, z):
@@ -14,7 +15,6 @@ def bust(x, y, z):
 
 
 def main():
-    import random
     num1 = random.randint(1, 11)
     num2 = random.randint(1, 11)
     num3 = random.randint(1, 11)
